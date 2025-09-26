6161042f9036a9ccd57eebcbc4377212e02333c5
Manfred <manfred@mac.com>
2025-09-26 21:29:58 +0200

    Removes chat functionality
    Removes the chat feature, including related PHP, JavaScript, and HTML files. This feature is no longer required.

--- chat/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// api/config.php
-// Pas deze waarden aan naar je eigen MySQL gegevens
-define('DB_HOST', 'localhost');
-define('DB_USER', 'flea_test');
-define('DB_PASS', 'Baardt123');
-define('DB_NAME', 'flea_test');
